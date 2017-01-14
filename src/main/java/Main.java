a2a8196af2146e9e115a06fb7df5510d9b0501b5
Farshid <farshid_11@hotmail.com>
2017-01-14 14:23:40 +0100

    Attribute Range Rule gemaakt en voorbeeld in de main voor de SQL query

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import model.BusinessRule;
+import model.BusinessRules.AttributeRange;
+
 public class Main {
     public static void main(String[] args) {
+        BusinessRule rule1 = new AttributeRange("Test", "constr_test", "prijs", false, 12, 15);
 
+        System.out.println(rule1);
     }
 }
