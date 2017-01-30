ae4013695c285b628786bff9eac4ae7d9deb3ed8
aversio <ua_gs_70@hotmail.com>
2017-01-30 18:32:39 +0100

    Fix AttributeRangeGenerator

--- src/main/java/org/hu/brg/generator/oracle/AttributeRangeGenerator.java
+++ src/main/java/org/hu/brg/generator/oracle/AttributeRangeGenerator.java
@@ -3,7 +3,10 @@ package org.hu.brg.generator.oracle;
 import org.hu.brg.generator.DatabaseType;
 import org.hu.brg.generator.Generator;
 import org.hu.brg.model.BusinessRule;
+import org.hu.brg.model.business_rule.AttributeCompare;
 import org.hu.brg.model.business_rule.AttributeRange;
+
+import javax.smartcardio.ATR;
 
 public class AttributeRangeGenerator extends Generator {
     public AttributeRangeGenerator() {
@@ -17,6 +20,28 @@ public class AttributeRangeGenerator extends Generator {
 
     @Override
     public String getOutput(BusinessRule businessRule) {
-        return "...OUTPUT ATTRIBUTE RANGE BUSINESS RULE...";
+        if (canHandle(businessRule)) {
+            AttributeCompare rule = (AttributeCompare) businessRule;
+
+            String[] comparisonOperators = {"=", "!=", "^=", "<>", ">", ">=", "<="};
+
+            String result = "";
+            for (String op : comparisonOperators) {
+                if (op.equals(rule.getOperator())) {
+                    result = rule.getOperator();
+                }
+            }
+
+            String sql = "ALTER TABLE " + rule.getTable() + "\n" +
+                    "ADD CONSTRAINT " + rule.getConstraint() + "\n" +
+                    "CHECK (" + rule.getAttribute() + " " + rule.getOperator() + " " + rule.getValue() + ");";
+
+            if (result.equals("")) {
+                return "Failed SQL generation.";
+            } else {
+                return sql;
+            }
+        }
+        return "Is no instance of BusinessRule";
     }
 }
